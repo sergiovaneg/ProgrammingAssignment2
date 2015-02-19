e7a3c32ce2a7d1d60ad050d611b6bb950ff2db83
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2015-02-19 16:04:14 -0500

    Modified version for Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function() m <<- solve
+  getmatrix <- function() m
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=makeCacheMatrix(), ...) {
+  m <- x$getmatrix()
+  if(!is.null(m)) {
+    print("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data,...)
+  x$setmatrix(m)
+  return(m)
 }
+
+## After doing this assignment, I've tried to run it, and even when it should be working, it doesn't.
+## Also, I tried to run the example (Caching the Mean of a Vector), and it doesn't work either.
+## I looked for help in Stackoverflow, but it didn't help.
+## Pd.: I'm running Rstudio with the R version 3.0.2 in a x86_64-pc-linux-gnu.
